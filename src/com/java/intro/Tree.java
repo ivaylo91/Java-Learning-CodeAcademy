371b3aab53054098abf0f60139b2766bf64ad51f
ivaylo91 <ipenev91@gmail.com>
2021-11-10 17:21:39 +0200

    Simple Projects Java

---
+++ src/com/java/intro/Tree.java
@@ -0,0 +1,14 @@
+package com.java.intro;
+
+public class Tree {
+    public static void main(String[] args) {
+        // Plant Tree
+        System.out.println("Hey there, I’m Ivaylo and I’m learning to code in Java!");
+        System.out.println("I’m going to plant a tree today!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
