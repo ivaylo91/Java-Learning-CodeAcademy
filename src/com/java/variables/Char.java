4e51d4341ebb08abbe3529b1368a1863d589d6b0
ivaylo91 <ipenev91@gmail.com>
2021-11-12 03:49:03 +0200

    char datatype

---
+++ src/com/java/variables/Char.java
@@ -0,0 +1,12 @@
+package com.java.variables;
+
+public class Char {
+    public static void main(String[] args) {
+
+        // Declare char variable
+        char expectedGrade = 'A';
+
+        //Print it to console
+        System.out.println(expectedGrade);
+    }
+}
