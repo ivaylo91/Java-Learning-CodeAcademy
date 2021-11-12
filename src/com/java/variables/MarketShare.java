67d44d1513551cc8272026645c8b56aae761807c
ivaylo91 <ipenev91@gmail.com>
2021-11-12 03:37:16 +0200

    Double Variable uses

---
+++ src/com/java/variables/MarketShare.java
@@ -0,0 +1,10 @@
+package com.java.variables;
+
+public class MarketShare {
+    public static void main(String[] args) {
+        // Declare double variable
+        double androidShare = 81.7;
+        // Print variable to console
+        System.out.println(androidShare);
+    }
+}
