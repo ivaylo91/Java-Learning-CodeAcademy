1aceb14f8fdb65adb29b4c6020ff2b7564db2470
ivaylo91 <ipenev91@gmail.com>
2021-11-12 03:43:15 +0200

    Boolean Variable use

---
+++ src/com/java/variables/Booleans.java
@@ -0,0 +1,12 @@
+package com.java.variables;
+
+public class Booleans {
+    public static void main(String[] args) {
+
+        //Declare boolean variable
+        boolean intsCanHoldDecimals = false;
+
+        //Print it to the console
+        System.out.println(intsCanHoldDecimals);
+    }
+}
