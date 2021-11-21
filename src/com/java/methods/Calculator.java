4ad961e4144f315a2d1fa70dd4bee66910e0d22a
ivaylo91 <ipenev91@gmail.com>
2021-11-21 21:52:07 +0200

    Simple Calculator project

---
+++ src/com/java/methods/Calculator.java
@@ -0,0 +1,35 @@
+package com.java.methods;
+
+public class Calculator {
+    public Calculator() {
+
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int modulo(int a, int b) {
+        return a % b;
+    }
+
+    public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
+        int add = myCalculator.add(5, 7);
+        System.out.println(add);
+        int subtract = myCalculator.subtract(45,11);
+        System.out.println(subtract);
+    }
+}
